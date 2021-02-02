7f1cd0a97b4eeae1e502cfc14e390beba908768d
nikhil <nikhilatoliya12@gmail.com>
2021-02-02 19:31:31 +0530

    added

---
+++ src/setupFirebase.js
@@ -0,0 +1,9 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+
+// create a web app in fireabse 
+// then get your app config object from firebase console -> settings
+const firebaseConfig = {};
+
+firebase.initializeApp(firebaseConfig);
